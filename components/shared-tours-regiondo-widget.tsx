8974dc9a49490813f8c4f0ac27c041b5206889c6
Alex Corazza <alex.corazza@icloud.com>
2026-06-03 01:40:09 +0200

    feat: regiondo upd

--- components/shared-tours-regiondo-widget.tsx
+++ components/shared-tours-regiondo-widget.tsx
@@ -1,72 +1,36 @@
 "use client";
 
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useLocale } from "next-intl";
+
+const widgetIds: Record<string, string> = {
+  en: "7365a711-ca3c-4834-8686-e19642235ae2",
+  it: "7365a711-ca3c-4834-8686-e19642235ae2",
+  zh: "e57d7f00-a3e8-446b-b873-8f236fcf8ae2",
+  ja: "f1ecfc87-53bd-4e5d-aff5-7ab7abf64bba",
+};
 
 export function SharedToursRegiondoWidget() {
   const locale = useLocale();
+  const widgetId = widgetIds[locale] ?? widgetIds.en;
 
   useEffect(() => {
-    if (locale === "zh" || locale === "ja") {
-      const src =
-        "https://www.regiondo.com/js/integration/calendarwidget/calendarwidget.js";
-      if (!document.querySelector(`script[src="${src}"]`)) {
-        const s = document.createElement("script");
-        s.src = src;
-        s.type = "text/javascript";
-        s.async = true;
-        document.body.appendChild(s);
-      }
-    } else {
-      const src =
-        "https://widgets.regiondo.net/catalog/v1/catalog-widget.min.js";
-      if (!document.querySelector(`script[src="${src}"]`)) {
-        const s = document.createElement("script");
-        s.src = src;
-        s.type = "text/javascript";
-        s.async = true;
-        document.body.appendChild(s);
-      }
+    const src = "https://widgets.regiondo.net/catalog/v1/catalog-widget.min.js";
+    if (!document.querySelector(`script[src="${src}"]`)) {
+      const s = document.createElement("script");
+      s.src = src;
+      s.type = "text/javascript";
+      s.async = true;
+      document.body.appendChild(s);
     }
-  }, [locale]);
-
-  if (locale === "zh") {
-    return (
-      <div
-        id="regiondo-calendar-widget"
-        className="w-full h-full"
-        data-code="46886-d9d1a36ce3a1fe350ea04fdc3be5235e"
-        data-locale="zh_CN"
-        data-apiurl="https://www.regiondo.com/"
-        data-sf="0"
-        data-sp="1"
-        data-vt="m"
-      />
-    );
-  }
-
-  if (locale === "ja") {
-    return (
-      <div
-        id="regiondo-calendar-widget"
-        className="w-full h-full"
-        data-code="46886-1e2085fb44369bc974a8a5e8e92caa47"
-        data-locale="ja_JP"
-        data-apiurl="https://www.regiondo.com/"
-        data-sf="0"
-        data-sp="1"
-        data-vt="m"
-      />
-    );
-  }
+  }, []);
 
   return (
     <div
       id="regiondo-widget"
       className="w-full h-full"
       dangerouslySetInnerHTML={{
-        __html:
-          '<product-catalog-widget widget-id="7365a711-ca3c-4834-8686-e19642235ae2"></product-catalog-widget>',
+        __html: `<product-catalog-widget widget-id="${widgetId}"></product-catalog-widget>`,
       }}
     />
   );
